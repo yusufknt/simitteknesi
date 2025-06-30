2a399162a925d7132cf6ecc119812ee38d336343
Yusuf Kantarcıoğlu <yusufkantarcioglu@Yusuf-MacBook-Air.local>
2025-07-01 00:24:02 +0300

    Add critters package and remove experimental CSS optimization

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const nextConfig = {
     domains: ["images.unsplash.com", "via.placeholder.com"],
     unoptimized: true,
   },
-  experimental: {
-    optimizeCss: true,
-  },
+
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|svg|webp)$/i,
